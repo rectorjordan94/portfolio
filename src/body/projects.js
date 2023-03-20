1e8d00a45be8553b551f538f77d9ee7a6fd1e18c
Jordan <rectorjordan94@gmail.com>
2023-03-20 13:04:58 -0500

    adds image carousel to showProjectModal

--- src/body/projects.js
+++ src/body/projects.js
@@ -9,14 +9,16 @@ const Projects = (props) => {
             description: 'Remake of the classic arcade game, playable directly in the browser. Features keyboard & button inputs for player movement & projectiles, randomly generated environmental obstacles, level progression, player lives, game reset, & scoring. Developed wtih HTML, CSS, JavaScript, and canvas.',
             thumbnail: 'https://github.com/rectorjordan94/Centipede/raw/main/images/centipede_score.png',
             github: 'https://github.com/rectorjordan94/Centipede',
-            deployed: 'https://rectorjordan94.github.io/Centipede/'
+            deployed: 'https://rectorjordan94.github.io/Centipede/',
+            images: ['https://github.com/rectorjordan94/Centipede/raw/main/images/centipede_start.png', 'https://github.com/rectorjordan94/Centipede/raw/main/images/centipede_play.png', 'https://github.com/rectorjordan94/Centipede/raw/main/images/centipede_score.png']
         },
         {
             name: 'Everdell-Wiki',
             description: "Collaborative, full-stack wiki app made in the MVC system with authentication, authorization, and full CRUD on the app's main resource. Built with HTML, CSS, JavaScript, Node.js, Express, MongoDB, Mongoose, Bootstrap, & LiquidJS.",
             thumbnail: 'https://github.com/rectorjordan94/everdell-wiki/raw/main/images/CARDS1.png',
             github: 'https://github.com/rectorjordan94/everdell-wiki',
-            deployed: 'https://everdellwiki.fly.dev/'
+            deployed: 'https://everdellwiki.fly.dev/',
+            images: ['https://github.com/rectorjordan94/everdell-wiki/raw/main/images/HOME1.png', 'https://github.com/rectorjordan94/everdell-wiki/raw/main/images/CARDS1.png', 'https://github.com/rectorjordan94/everdell-wiki/raw/main/images/SHOWCARD1.png', 'https://github.com/rectorjordan94/everdell-wiki/raw/main/images/NEW1.png']
         },
         {
             name: 'hoist{m}',
@@ -24,15 +26,17 @@ const Projects = (props) => {
             thumbnail: 'https://github.com/rectorjordan94/hoistm-client/raw/main/images_readme/label-filter.png',
             frontendGitHub: 'https://github.com/rectorjordan94/hoistm-client',
             backendGitHub: 'https://github.com/rectorjordan94/hoistm-api',
-            deployed: 'https://hoistm.netlify.app/'
+            deployed: 'https://hoistm.netlify.app/',
+            images: ['https://github.com/rectorjordan94/hoistm-client/raw/main/images_readme/sign-in.png', 'https://github.com/rectorjordan94/hoistm-client/raw/main/images_readme/HOME.png', 'https://github.com/rectorjordan94/hoistm-client/raw/main/images_readme/label-filter.png', 'https://github.com/rectorjordan94/hoistm-client/raw/main/images_readme/show-file-modal.png', 'https://github.com/rectorjordan94/hoistm-client/raw/main/images_readme/edit-file-modal.png']
         },
         {
             name: 'BLINK',
             description: "Full-stack Slack clone where users can create and join different channels and chat with other users in real-time. Built with the MERN stack using the MVC system for organizing the code. Utilizes socket.io for bidirectional event-based communication between clients and server for chat functionality. Created with HTML, CSS, JavaScript, React, MongoDB, Mongoose, Express, and Node.js.",
             thumbnail: 'https://github.com/rectorjordan94/blink-api/raw/main/images/channel.png',
-            frontendGitHub: 'https://github.com/rectorjordan94/hoistm-client',
-            backendGitHub: 'https://github.com/rectorjordan94/hoistm-api',
-            deployed: 'https://hoistm.netlify.app/'
+            frontendGitHub: 'https://github.com/rectorjordan94/blink-client',
+            backendGitHub: 'https://github.com/rectorjordan94/blink-api',
+            deployed: 'https://main--gorgeous-cranachan-8bbf33.netlify.app/',
+            images: ['https://github.com/rectorjordan94/blink-api/raw/main/images/channel.png']
         }
     ]
 
@@ -65,23 +69,19 @@ const Projects = (props) => {
                 <h2 className="pt-3 pb-1 section-header">Projects</h2>
                 <hr className="line" />
                 <div className="row mx-1">
-                    <div className="col paragraph me-3">
-                        <h4 className="project-header">Centipede</h4>
+                    <div className="col paragraph me-3 d-flex">
                         <button className='project-button' value={JSON.stringify(myProjects[0])} onClick={onClick}><img src={myProjects[0].thumbnail} alt="centipede thumbnail" className="w-100 image" style={{pointerEvents: 'none'}}/></button>
                     </div>
-                    <div className="col paragraph">
-                        <h4 className="project-header">Everdell-Wiki</h4>
-                        <a href="https://github.com/rectorjordan94/everdell-wiki"><img src={myProjects[1].thumbnail} alt="" className="w-100 image"/></a>
+                    <div className="col paragraph d-flex">
+                        <button className='project-button' value={JSON.stringify(myProjects[1])} onClick={onClick}><img src={myProjects[1].thumbnail} alt="everdell wiki thumbnail" className="w-100 image" style={{pointerEvents: 'none'}}/></button>
                     </div>
                 </div>
                 <div className="row mx-1 my-3">
-                    <div className="col paragraph me-3">
-                        <h4 className="project-header">hoist&#123;m&#125;</h4>
-                        <a href="https://github.com/rectorjordan94/hoistm-client"><img src={myProjects[2].thumbnail} alt="" className="w-100 image"/></a>
+                    <div className="col paragraph me-3 d-flex">
+                        <button className='project-button' value={JSON.stringify(myProjects[2])} onClick={onClick}><img src={myProjects[2].thumbnail} alt="hoist thumbnail" className="w-100 image" style={{pointerEvents: 'none'}}/></button>
                     </div>
-                    <div className="col paragraph">
-                        <h4 className="project-header">BLINK</h4>
-                        <a href="https://github.com/rectorjordan94/blink-api"><img src={myProjects[3].thumbnail} alt="" className="w-100 image"/></a>
+                    <div className="col paragraph d-flex">
+                        <button className='project-button' value={JSON.stringify(myProjects[3])} onClick={onClick}><img src={myProjects[3].thumbnail} alt="blink thumbnail" className="w-100 image" style={{pointerEvents: 'none'}}/></button>
                     </div>
                 </div>
             </div>
--- src/projects/ShowProjectModal.js
+++ src/projects/ShowProjectModal.js
@@ -1,14 +1,52 @@
+import { NavItem } from "react-bootstrap"
 import Modal from "react-bootstrap/Modal"
 
 const ShowProjectModal = (props) => {
     const { show, project, handleClose } = props
 
     if (project) {
+
+        const carouselImages = []
+
+        project.images.forEach((element, i) => {
+            if (i === 0) {
+                carouselImages.push(<div class="carousel-item active">
+                    <img src={element} key={i} className="d-block w-100" alt="carousel"/>
+                </div>)
+            } else {
+                carouselImages.push(<div class="carousel-item">
+                    <img src={element} key={i} className="d-block w-100" alt="carousel"/>
+                </div>)
+            }
+        })
+
         return (
             <Modal show={show} onHide={handleClose} id='project-modal'>
                 <Modal.Header closeButton id='project-modal-header' closeVariant='white'>
                     <Modal.Title style={{color: '#000000'}}>{project.name}</Modal.Title>
                 </Modal.Header>
+                <Modal.Body>
+                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            {carouselImages}
+                        </div>       
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                    </div>
+                    <p>{project.description}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    {project.github ? <a href={project.github} target="_blank">GitHub</a> : null}
+                    {project.frontendGitHub ? <a href={project.frontendGitHub} target="_blank">Client GitHub</a> : null}
+                    {project.backendGitHub ? <a href={project.backendGitHub} target="_blank">Api GitHub</a> : null}
+                    <a href={project.deployed} target="_blank">Deployed</a>
+                </Modal.Footer>
             </Modal>
         )
     }
