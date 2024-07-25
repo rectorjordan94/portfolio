260527263b9ea2967bd665190111311da5705e53
Jordan <rectorjordan94@gmail.com>
2024-07-25 16:03:04 -0700

    changes navbar back to dropdown style, makes background transparent, still has placeholder items

--- src/Header.js
+++ src/Header.js
@@ -16,36 +16,21 @@ const Header = (props) => {
         //         </div>
         //     </div>
         // </>
-        <nav class="navbar navbar-dark bg-body-tertiary fixed-top">
-            <div class="container-fluid align-items-start">
-                <a class="navbar-brand" href="#"><img src="/images/1.png" alt="logo" id="logo"/></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary container-fluid">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 text-start">
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">LinkedIn</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">GitHub</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Fortnite Creator</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Resume</a>
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                     </ul>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     )
 }
 
