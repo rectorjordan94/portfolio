939f1f2d4f1de57e2c286ea5e28ac8cb40b8b2aa
Jordan <rectorjordan94@gmail.com>
2024-07-24 18:27:56 -0700

    adds media queries, updates about component's html to be semantic

--- src/body/about.js
+++ src/body/about.js
@@ -1,13 +1,13 @@
 const About = (props) => {
 
     return (
-        <div className="section">
+        <section>
             <div className="container">
                 <h1 id="about-name">Jordan Rector</h1>
                 <h2 id="about-title">software developer</h2>
                 <p id="about-paragraph">Full-stack software engineer & web developer with a diverse background in hospitality and game design. Passionate about solving complex problems with a knack for creative solutions. Dedicated to crafting user-friendly experiences that tackle modern challenges head-on. Actively pursuing ongoing education and skill development to stay ahead in an ever-evolving professional landscape.</p>
             </div>
-        </div>
+        </section>
     )
 }
 
