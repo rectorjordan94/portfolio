c1c728f649c9e7cd50955496fca6187051c35985
Jordan <rectorjordan94@gmail.com>
2024-07-26 16:06:26 -0700

    styles about modal header and preliminary styling for main content and footer

--- src/body/Bio.js
+++ src/body/Bio.js
@@ -2,22 +2,22 @@ const Bio = (props) => {
 
 	return (
 		<>
-			<button type="button" class="btn btn-primary modal-nav-button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+			<button type="button" class="btn btn-primary modal-nav-button" data-bs-toggle="modal" data-bs-target="#aboutmodal">
 			<span className="nav-symbols">&#172;</span>about
 			</button>
-			<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal fade" id="aboutmodal" tabindex="-1" aria-labelledby="aboutModallabel" aria-hidden="true">
 			<div class="modal-dialog modal-fullscreen modal-dialog-scrollable">
     			<div class="modal-content">
-      				<div class="modal-header">
-        				<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+      				<div class="modal-header" id="about-modal-header">
+        				<h5 class="modal-title" id="about-modal-title">ABOUT</h5>
         				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       				</div>
-      				<div class="modal-body">
-        				...
+      				<div class="modal-body text-dark">
+        				This is where I'm gonna type some stuff and things for sure
       				</div>
       				<div class="modal-footer">
-        				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        				<button type="button" class="btn btn-primary">Save changes</button>
+        				<button type="button" class="btn btn-success" data-bs-dismiss="modal">Close</button>
+        				<button type="button" class="btn btn-info">Save changes</button>
       				</div>
     			</div>
   			</div>
