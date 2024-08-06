7ac3a7af1a8e065abd7a3ca28428db14ac6ac413
Jordan <rectorjordan94@gmail.com>
2024-08-06 16:14:59 -0700

    replaces and adds new square bio photos for carousel on about page

--- src/body/Bio.js
+++ src/body/Bio.js
@@ -41,60 +41,74 @@ const Bio = (props) => {
 							</div>
 							<div id="bio-img-container">
 							<div id="carouselFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
-								<div className="carousel-inner">
-									<div className="carousel-item active">
-										<img src="/bio_photos/244055931_604682570718909_6364658061903751902_n.jpg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/mexcio.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/364260916_829877328745583_4285141526066776034_n.jpg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/atitlan3.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/chiapas.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/ice7.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/ice.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/st_louis.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/ice4.jpeg" class="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/switz.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/sequoia.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/sanfran.jpeg" className="d-block w-100 bio-img" alt="..."></img>
-									</div>
-									<div className="carousel-item">
-										<img src="/bio_photos/ice3.jpeg" className="d-block w-100 bio-img" alt="..."></img>
+									<div className="carousel-inner">
+										<div className="carousel-item active">
+											<img src="/bio_photos/whitney.png" className="d-block w-100 bio-img" alt="A candid shot of me overlooking the edge of a mountain"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/wingspan.png" className="d-block w-100 bio-img" alt="The colorful components of the board game Wingspan, nearly covering an entire table"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/3D.png" className="d-block w-100 bio-img" alt="An abstract 3D model made in UEFN, white and spaceship-like"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/atitlan.png" className="d-block w-100 bio-img" alt="A colorful street near Lake Atitlan in Guatemala, decorated with hanging decorations in preparation for a festival"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/bar.png" className="d-block w-100 bio-img" alt="The well behind a bar I worked at, full of color-coded and well-organized squeeze bottles of various syrups"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/mall.png" className="d-block w-100 bio-img" alt="A clean and modern mall built in UEFN"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/chiapas.png" className="d-block w-100 bio-img" alt="A lake surrounded by green mountains in Chiapas, Mexico with some lavender flowers in the foreground"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/massive.png" className="d-block w-100 bio-img" alt="A sprawling scene depicting many of the components laid out on a table for the board game Massive Darkness 2"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/perspective.png" className="d-block w-100 bio-img" alt="An abstract geometric drawing in black and white, depicting a scene like 4 identical skyscrapers looming above"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/grafitti.png" className="d-block w-100 bio-img" alt="Vivid street art on the side of a building in Chicago"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/medieval.png" className="d-block w-100 bio-img" alt="A small medieval town square at sunset, built in UEFN"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/lights.png" className="d-block w-100 bio-img" alt="Green northern lights over a well-lit cabin in Iceland"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/spire.png" className="d-block w-100 bio-img" alt="The defect's character board from the slay the spire board game"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/tree.png" className="d-block w-100 bio-img" alt="An abstract, futuristic 3D model of a tree, with silver trunk and branches, and bright orange leaves, built in UEFN"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/reflection.png" className="d-block w-100 bio-img" alt="A bay with sailboats during sunset, the water so still it perfectly mirrors the sky above"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/shield.png" className="d-block w-100 bio-img" alt="The material editor in UEFN, with a preview of a holographic and animated shield material"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/switzerland.png" className="d-block w-100 bio-img" alt="A lush and rocky valley in Switzerland, high up enough that clouds are lower than some of the peaks in the distance"></img>
+										</div>
+										<div className="carousel-item">
+											<img src="/bio_photos/visual_greys.png" className="d-block w-100 bio-img" alt="A drawing of overlapping spheres that are only defined by shading near their edges achieved using pointilism"></img>
 										</div>
 										<button className="carousel-control-prev" type="button" data-bs-target="#carouselFade" data-bs-slide="prev">
-									<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-									<span className="visually-hidden">Previous</span>
-								</button>
-								<button className="carousel-control-next" type="button" data-bs-target="#carouselFade" data-bs-slide="next">
-									<span className="carousel-control-next-icon" aria-hidden="true"></span>
-									<span className="visually-hidden">Next</span>
-								</button>
+											<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+											<span className="visually-hidden">Previous</span>
+										</button>
+										<button className="carousel-control-next" type="button" data-bs-target="#carouselFade" data-bs-slide="next">
+											<span className="carousel-control-next-icon" aria-hidden="true"></span>
+											<span className="visually-hidden">Next</span>
+										</button>
+									</div>
 								</div>
 							</div>
-							
-							</div>
 							<div className="bio-container">
-								<p className="bio-paragraph">Outside of work, I'm a bit of a hobby hoarder. From photography and cooking, to hiking and gaming, I'm always diving into new activities. I love learning new things--whether it's a new recipe, picking up a coding language, or theorycrafting builds for the games I'm playing.</p>
+								<p className="bio-paragraph">Outside of work, I'm a bit of a hobby hoarder. From cooking and reading, to hiking and gaming, I'm always diving into new activities. I love learning new things--whether it's a new recipe, picking up a coding language, or theorycrafting builds for the games I'm playing.</p>
 							</div>	
 							<div className="bio-container">
 								<p className="bio-paragraph">I'm really passionate about games. I think you can learn a lot about someone by watching them play or playing alongside them. I'm fascinated by how people make decisions and what drives their choices, especially in games.</p>
