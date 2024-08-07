e6e9132c4fdb98ee0c75a3cbc4064455a369bcb8
Jordan <rectorjordan94@gmail.com>
2024-08-07 16:27:38 -0700

    moves bio intro into a separate p tag so it can be styled differenly, adjusts paddings of bio-containers

--- src/body/Bio.js
+++ src/body/Bio.js
@@ -34,7 +34,8 @@ const Bio = (props) => {
       				</div>
 						<div className="modal-body text-dark" id="bio-modal-body">
 							<div className="bio-container">
-								<p className="bio-paragraph"><span id="bio-intro">Hey, I'm Jordan!</span> I'm a software and game developer with a knack for creative problem-solving and a keen eye for design thanks to my diverse background and wide-ranging interests.</p>
+								<p id="bio-intro">Hey, I'm Jordan!</p>
+								<p className="bio-paragraph"> I'm a software and game developer with a knack for creative problem-solving and a keen eye for design thanks to my diverse background and wide-ranging interests.</p>
 							</div>
 							<div className="bio-container">
 								<p className="bio-paragraph">I've had the chance to wear many hats over the years: developer, artist, manager, photographer, bartender, director, designer, you name it. That experience made me highly adaptable and gave me plenty of opportunities to be creative while tackling various challenges. This mix of creativity and problem-solving is what led me to software and game development. Now I'm using those skills to create fun and impactful projects.</p>
